00b3e783dd48f89d800316e2f3fd7a9ab1388de2
Max Leibovich <138369969+max-zengo@users.noreply.github.com>
2024-03-17 13:34:53 +0200

    macros - macros compiling

--- src/macros.rs
+++ src/macros.rs
@@ -2,21 +2,22 @@ use rocket::info;
 #[macro_export]
 macro_rules! db_get {
     ($db:expr, $customer_id:expr, $id:expr, $enum_ident:ident) => {
-        $db.get(
+        match ($db.get(
             &crate::types::DbIndex {
                 customerId: $customer_id.to_string(),
                 id: $id.to_string(),
             },
             &EcdsaStruct::$enum_ident,
         )
-        .await
-        .unwrap_or_else(|err| { panic!(
+        .await) {
+            Ok(val) => val,
+            Err(err) =>  return Err(format!(
             "Failed to get from {} with customerId: {}, id: {} with error:\n{:?}",
             stringify!($enum_ident),
             $id,
             $customer_id,
-            err
-        )})
+            err))
+        }
     }
 }
 
@@ -28,27 +29,29 @@ macro_rules! db_get {
 #[macro_export]
 macro_rules! db_get_required {
     ($db:expr, $customer_id:expr, $id:expr, $enum_ident:ident) => {
-        $db.get(
+        match ($db.get(
             &crate::types::DbIndex {
                 customerId: $customer_id.to_string(),
                 id: $id.to_string(),
             },
             &EcdsaStruct::$enum_ident,
         )
-        .await
-        .unwrap_or_else(|err| { panic!(
+        .await) {
+            Ok(Some(val)) => val,
+            Ok(None) => return Err(format!(
+            "Value from {} with customerId: {}, id: {} is required",
+            stringify!($enum_ident),
+            $customer_id,
+            $id,
+            )),
+            Err(err) => return Err(format!(
             "Failed to get from {} with customerId: {}, id: {} with error:\n{:?}",
             stringify!($enum_ident),
             $customer_id,
             $id,
             err
-        )})
-        .unwrap_or_else(|| { panic!(
-            "Value from {} with customerId: {}, id: {} is required",
-            stringify!($enum_ident),
-            $customer_id,
-            $id,
-        )})
+            )),
+        }
     }
 }
 
@@ -59,7 +62,7 @@ macro_rules! db_get_required {
 #[macro_export]
 macro_rules! db_insert {
     ($db:expr, $customer_id:expr, $id:expr, $enum_ident:ident, $new_value:expr) => {
-        $db.insert(
+        match $db.insert(
             &crate::types::DbIndex {
                 customerId: $customer_id.to_string(),
                 id: $id.to_string(),
@@ -67,14 +70,23 @@ macro_rules! db_insert {
             &crate::types::EcdsaStruct::$enum_ident,
             $new_value,
         )
-        .await
-        .unwrap_or_else(|err| { panic!(
+        .await {
+            Ok(_) => (),
+            Err(err) => return Err(format!(
             "Failed to insert into {} with customerId: {}, id: {} with error:\n{:?}",
             stringify!($enum_ident),
             $customer_id,
             $id,
             err
-        )})
+        ))
+        }
+        // .unwrap_or_else(|err| { panic!(
+        //     "Failed to insert into {} with customerId: {}, id: {} with error:\n{:?}",
+        //     stringify!($enum_ident),
+        //     $customer_id,
+        //     $id,
+        //     err
+        // )})
     };
 }
 
