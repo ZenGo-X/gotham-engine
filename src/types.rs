9a499806b4217f6e9b875c6b78ba629d9791e8c4
Max Leibovich <138369969+max-zengo@users.noreply.github.com>
2024-04-09 13:16:07 +0300

    Revert "remove require_customer_id from EcdsaStruct"
    This reverts commit 2f926c57f91dfe7ad4044730440eb4c885eb3543.

--- src/types.rs
+++ src/types.rs
@@ -130,6 +130,10 @@ impl MPCStruct for EcdsaStruct {
         }
     }
 
+    fn require_customer_id(&self) -> bool {
+        self.to_string() == "Party1MasterKey" || self.to_string() == "Abort"
+    }
+
     // TODO: Add unit tests for below casting
     fn to_struct_name(&self) -> String {
         let res = match self {
