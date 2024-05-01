e74d12b3f7d1a270c2ce0009d7481ae424e8aca9
Max Leibovich <138369969+max-zengo@users.noreply.github.com>
2024-05-01 11:16:51 +0300

    add RotationParty1Message2 struct

--- src/server/rotate.rs
+++ src/server/rotate.rs
@@ -7,9 +7,7 @@ use tokio::sync::Mutex;
 use two_party_ecdsa::curv::cryptographic_primitives::twoparty::coin_flip_optimal_rounds;
 use two_party_ecdsa::curv::elliptic::curves::traits::ECScalar;
 use two_party_ecdsa::kms::ecdsa::two_party::MasterKey1;
-use two_party_ecdsa::kms::rotation::two_party::party1::{
-    RotateCommitMessage1, Rotation1, RotationParty1Message1,
-};
+use two_party_ecdsa::kms::rotation::two_party::party1::{RotateCommitMessage1, Rotation1, RotationParty1Message1, RotationParty1Message2};
 use two_party_ecdsa::kms::rotation::two_party::Rotation;
 use two_party_ecdsa::{party_one, party_two};
 use two_party_ecdsa::party_one::{Party1PDLDecommit, Party1Private};
@@ -45,15 +43,7 @@ pub trait Rotate {
         claim: Claims,
         id: String,
         coin_flip_party2_first: Json<coin_flip_optimal_rounds::Party2FirstMessage>,
-    ) -> Result<
-        Json<
-            Option<(
-                coin_flip_optimal_rounds::Party1SecondMessage,
-                RotationParty1Message1,
-            )>,
-        >,
-        String,
-    > {
+    ) -> Result<Json<RotationParty1Message2>, String> {
         let db = state.lock().await;
 
         let rotate_commit_message = db_get_required!(db, claim.sub, id, RotateCommitMessage1, RotateCommitMessage1);
@@ -67,30 +57,38 @@ pub trait Rotate {
             &party_one_master_key.private,
             &random1.rotation.to_big_int(),
         ) {
-            // TODO: check if RotateCommitMessage1M and RotateCommitMessage1R need to be deleted
-            return Ok(Json(None));
+            return Ok(Json(
+                RotationParty1Message2 {
+                    coin_flip_party1_second_message: None,
+                    rotation_party1_prev_message: None,
+                    is_valid: false,
+                }
+            ));
         }
 
         db_insert!(db, claim.sub, id, RotateRandom1, &random1);
 
-        let (rotation_party_one_first, party_one_private_new) =
+        let (rotation_party1_first, party_one_private_new) =
             party_one_master_key.rotation_first_message(&random1);
 
-        db_insert!(db, claim.sub, id, RotateFirstMsg, &rotation_party_one_first);
+        db_insert!(db, claim.sub, id, RotateFirstMsg, &rotation_party1_first);
 
         db_insert!(db, claim.sub, id, RotatePrivateNew, &party_one_private_new);
 
-        Ok(Json(Some((
-            coin_flip_party1_second,
-            rotation_party_one_first,
-        ))))
+        Ok(Json(
+            RotationParty1Message2 {
+                coin_flip_party1_second_message: Some(coin_flip_party1_second),
+                rotation_party1_prev_message: Some(rotation_party1_first),
+                is_valid: true,
+            }
+        ))
     }
 
     async fn rotate_third(
         state: &State<Mutex<Box<dyn Db>>>,
         claim: Claims,
         id: String,
-        rotation_party_two_first: Json<party_two::Party2PDLFirstMessage>,
+        rotation_party_two_first: Json<Party2PDLFirstMessage>,
     ) -> Result<Json<party_one::Party1PDLFirstMessage>, String> {
         let db = state.lock().await;
 
