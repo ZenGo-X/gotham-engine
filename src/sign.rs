7bedfb70b06d45367710c07845d5969299ad4dda
Max Leibovich <138369969+max-zengo@users.noreply.github.com>
2024-05-02 11:07:18 +0300

    Revert "bug fix: must be Some(claim.sub.clone()) in all but derive"
    This reverts commit 50e23ec5d36b577b2515a0f3170403203ac48608.

--- src/sign.rs
+++ src/sign.rs
@@ -27,7 +27,7 @@ pub trait Sign {
         let db = state.lock().await;
 
         // Abort table has only customerId as key
-        let tmp = db_get!(db, Some(claim.sub.clone()), Some(claim.sub.clone()), Abort)
+        let tmp = db_get!(db, Some(claim.sub.clone()), None::<String>, Abort)
             .unwrap_or(Box::new(Abort { blocked: false }));
         let to_abort = db_cast!(tmp, Abort);
 
@@ -39,9 +39,9 @@ pub trait Sign {
         let (sign_party_one_first_message, eph_ec_key_pair_party1) =
             MasterKey1::sign_first_message();
 
-        db_insert!(db, Some(claim.sub.clone()), Some(id.clone()), EphKeyGenFirstMsg, &eph_key_gen_first_message_party_two.0);
+        db_insert!(db, None::<String>, Some(id.clone()), EphKeyGenFirstMsg, &eph_key_gen_first_message_party_two.0);
 
-        db_insert!(db, Some(claim.sub.clone()), Some(id.clone()), EphEcKeyPair, &eph_ec_key_pair_party1);
+        db_insert!(db, None::<String>, Some(id.clone()), EphEcKeyPair, &eph_ec_key_pair_party1);
 
         Ok(Json(sign_party_one_first_message))
     }
@@ -71,10 +71,10 @@ pub trait Sign {
 
         //: party_one::EphEcKeyPair
 
-        let eph_ec_key_pair_party1 = db_get_required!(db, Some(claim.sub.clone()), Some(id.clone()), EphEcKeyPair, Party1EphEcKeyPair);
+        let eph_ec_key_pair_party1 = db_get_required!(db, None::<String>, Some(id.clone()), EphEcKeyPair, Party1EphEcKeyPair);
 
 
-        let eph_key_gen_first_message_party_two = db_get_required!(db, Some(claim.sub.clone()), Some(id.clone()), EphKeyGenFirstMsg, Party2EphKeyGenFirstMessage);
+        let eph_key_gen_first_message_party_two = db_get_required!(db, None::<String>, Some(id.clone()), EphKeyGenFirstMsg, Party2EphKeyGenFirstMessage);
 
         let signature_with_recid = child_master_key.sign_second_message(
             &request.party_two_sign_message,
@@ -86,7 +86,7 @@ pub trait Sign {
         match signature_with_recid {
             Ok(sig) => Ok(Json(sig)),
             Err(_) => {
-                db_insert!(db, Some(claim.sub.clone()), Some(id.clone()), Abort, &Abort { blocked: true });
+                db_insert!(db, None::<String>, Some(id.clone()), Abort, &Abort { blocked: true });
                 Err(format!("sign_second failed for customer_id {}, id {}. Inserted into Abort table",  claim.sub, id))
             }
         }
@@ -144,7 +144,7 @@ async fn sign_first_helper(
 ) -> Result<Json<(String, Party1EphKeyGenFirstMessage)>, String> {
     let db = state.lock().await;
 
-    let tmp = db_get!(db, Some(claim.sub.clone()), Some(claim.sub.clone()), Abort)
+    let tmp = db_get!(db, Some(claim.sub.clone()), None::<String>, Abort)
         .unwrap_or(Box::new(Abort { blocked: false }));
 
     let to_abort = db_cast!(tmp, Abort);
@@ -241,7 +241,7 @@ async fn sign_second_helper(
     match signature_with_recid {
         Ok(sig) => Ok(Json(sig)),
         Err(err) => {
-            db_insert!(db, Some(claim.sub.clone()), Some(id.clone()), Abort, &Abort { blocked: true });
+            db_insert!(db, None::<String>, Some(id.clone()), Abort, &Abort { blocked: true });
             Err(format!("sign_second failed for customer_id {:?}, ssid {:?}, id: {:?}, sid: {:?}. \
             Inserted into Abort table",  claim.sub, ssid, id, sid))
         }
