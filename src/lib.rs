89f55bbeae028710f78b49e3940bd6491f469083
Max Leibovich <138369969+max-zengo@users.noreply.github.com>
2024-05-06 19:50:45 +0300

    two_party_ecdsa_bridge

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,15 @@
 pub mod guarder;
 pub mod keygen;
-mod macros;
+pub mod macros;
 pub mod rotate;
 pub mod routes;
 pub mod sign;
 pub mod derive;
 pub mod traits;
 pub mod types;
+
+pub mod two_party_ecdsa_bridge {
+    pub use two_party_ecdsa::*;
+}
+
 // type Result<T> = std::result::Result<T, failure::Error>;
