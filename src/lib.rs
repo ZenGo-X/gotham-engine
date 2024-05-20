62b3085b29abc54877b06c760154e79260319a2b
Max Leibovich <138369969+max-zengo@users.noreply.github.com>
2024-05-20 11:11:51 +0300

    expose two-party-ecdsa as mod

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
 pub mod client;
 pub mod server;
 // type Result<T> = std::result::Result<T, failure::Error>;
+
+pub mod two_party_ecdsa {
+    pub use two_party_ecdsa::*;
+}
--- src/mod.rs
+++ src/mod.rs
@@ -1,2 +1,6 @@
-pub mod client;
-pub mod server;
+// pub mod client;
+// pub mod server;
+//
+// pub mod two_party_ecdsa {
+//     pub use two_party_ecdsa::*;
+// }
